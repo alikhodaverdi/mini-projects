4618c3da23f2a0910b8436fb45d60a4d5066a8a0
alk5 <alikhodaveerdi@yahoo.com>
2026-02-06 17:53:03 +0330

    All_links_from_given_webpage

---
+++ All_links_from_given_webpage/main.py
@@ -0,0 +1,20 @@
+import requests as rq 
+from bs4 import BeautifulSoup
+
+
+
+url = input("Enter Link:")
+if ("https" or "http") in url:
+    data = rq.get(url)
+    
+else:
+    data = rq.get(url)
+    
+soup = BeautifulSoup(data.text,"html.parser")
+links = []
+for link in soup.find_all("a"):
+    links.append(link.get("href"))
+    
+
+with open("mylinks.text","a") as saved:
+    print(links[:10], file=saved)
